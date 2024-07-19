832b3cd1a6e9f9146da4f67f8b18469bb11f763f
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-07-19 12:56:33 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nicolasgarin.github.io",
   base: "loopstudios",
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
